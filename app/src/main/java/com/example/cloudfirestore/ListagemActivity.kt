222a61b92d46f5bc86d5e8a7cb5d678cf9c421cb
pedrolima26-web <pedrohenrique.l.botelho@gmail.com>
2022-10-24 09:01:51 -0300

    excluir pronto!

--- app/src/main/java/com/example/cloudfirestore/ListagemActivity.kt
+++ app/src/main/java/com/example/cloudfirestore/ListagemActivity.kt
@@ -2,6 +2,8 @@ package com.example.cloudfirestore
 
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,16 +11,23 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class ListagemActivity : AppCompatActivity() {
+    private lateinit var alert: AlertDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listagem)
 
         // Access a Cloud Firestore instance from your Activity
         val db = Firebase.firestore
+
+
+        val tobaoEdita = findViewById<Button>(R.id.btEdita)
+        val tobaoExclui = findViewById<Button>(R.id.btExclui)
+
         val docRef = db.collection("users")
         val display = findViewById<TextView>(R.id.display)
         docRef.get().addOnSuccessListener { documents ->
@@ -34,6 +43,46 @@ class ListagemActivity : AppCompatActivity() {
 
 
 
+        }
+
+        tobaoExclui.setOnClickListener {
+            val build = AlertDialog.Builder(this)
+            val view = layoutInflater.inflate(R.layout.dialog_excluir,null)
+            build.setView(view)
+            val botao = view.findViewById<Button>(R.id.fechaaa)
+            val botao2 = view.findViewById<Button>(R.id.btExcluir)
+            botao.setOnClickListener {
+                alert.dismiss()
+            }
+
+            botao2.setOnClickListener {
+                val nome = view.findViewById<EditText>(R.id.edtEx).text.toString()
+                val doc = db.collection("users").whereEqualTo("Nome", nome).get()
+                    .addOnSuccessListener { documents ->
+                        if(!documents.isEmpty) {
+                            for (document in documents)
+                            db.collection("users").document(document.id).delete()
+                                .addOnSuccessListener {
+                                    Toast.makeText(baseContext, "Document Excluído", Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener {
+                                    Toast.makeText(baseContext, "Erro: Document Não Excluído", Toast.LENGTH_SHORT).show()
+                                }
+                        }
+                        }
+                    .addOnFailureListener {
+                        Toast.makeText(baseContext, "Erro: Document Não Encontrado", Toast.LENGTH_SHORT).show()
+                    }
+                //docRef.where()
+            }
+
+
+
+
+
+            alert = build.create()
+            alert.show()
+            alert.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         }
 
 
